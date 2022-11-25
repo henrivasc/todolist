d789465332da4d9ad3b39656d092925ae6e5666c
henrivasc <116586840+henrivasc@users.noreply.github.com>
2022-11-25 17:53:20 -0300

    Add files via upload
    primeiros funcionamentos

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print('Hello')
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+tasks = [
+    {'name': 'Estudar', 'finished': False},
+    {'name': 'Dormir', 'finished': True},
+    {'name': 'Comer', 'finished': True}
+]
+
+@app.route('/')
+def home():
+    # templates/home.html
+    return render_template('home.html', tasks=tasks)
+
+@app.route('/create', methods=['POST'])
+def create():
+    name = request.form['name']
+    task = {'name': name, 'finished': False}
+    tasks.append(task)
+    return render_template('home.html', tasks=tasks)
+    return name 
+
+app.run(debug=True)
+
